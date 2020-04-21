4e17a060d39c85bb3943e2259e56eef6d2353749
Len Chang <lamgo.chang@gmail.com>
2020-04-21 17:32:47 +0800

    Modern React with Redux: Section 14 - 162 to 170 - Async Actions with Redux Thunk

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PostList from "./PostList";
 
 const App = () => {
-  return <div className="ui container">Hello</div>;
+  return (
+    <div className="ui container">
+      <PostList></PostList>
+    </div>
+  );
 };
 
 export default App;
